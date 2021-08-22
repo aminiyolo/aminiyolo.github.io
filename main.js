16a9918ad4cfa338a99186f9aa2a4da06b1ace99
aminiyolo <73641636+aminiyolo@users.noreply.github.com>
2021-08-23 00:46:05 +0900

    Add Cursor

--- main.js
+++ main.js
@@ -10,3 +10,25 @@ window.onscroll = () => {
   menu.classList.remove("fa-times");
   header.classList.remove("active");
 };
+
+const cursor1 = document.querySelector(".cursor1");
+const cursor2 = document.querySelector(".cursor2");
+
+window.onmousemove = (e) => {
+  cursor1.style.top = e.pageY + "px";
+  cursor1.style.left = e.pageX + "px";
+  cursor2.style.top = e.pageY + "px";
+  cursor2.style.left = e.pageX + "px";
+};
+
+document.querySelectorAll("a").forEach((link) => {
+  link.onmouseenter = () => {
+    cursor1.classList.add("active");
+    cursor2.classList.add("active");
+  };
+
+  link.onmouseleave = () => {
+    cursor1.classList.remove("active");
+    cursor2.classList.remove("active");
+  };
+});
